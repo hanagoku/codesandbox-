41dcbc6b533906f702eeb83471b376d7242b2998
hanagoku <108511558+hanagoku@users.noreply.github.com>
2022-07-07 18:06:00 +0900

    REACT1 07071805

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import "./styles.css";
-
-export default function App() {
+import React from "react";
+const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue"
+  };
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <h1 style={{ color: "red" }}> こんにちは！ </h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
   );
-}
+};
+//ほかのファイルでも関数が使用できるように明示的に示す必要がある
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import React from "react";
+import ReactDom from "react-dom";
+//どこ～関数を持ってきたのかが分かるようにインポート時には関数と、ファイル名を書く
+import App from "./App.js";
+//画面にApp関数を反映,どこに反映かも（）の中に書く
+ReactDom.render(<App />, document.getElementById("root"));
